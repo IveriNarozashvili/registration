fc56ffca02ee894a75edee5085b34c1418abae04
IveriNarozashvili <geobmw91>
2020-04-04 18:47:41 +0300

    add user class

---
+++ src/User.java
@@ -0,0 +1,47 @@
+
+public class User {
+
+    private  String nickName;
+    private String firstName;
+    private String lastName;
+    private int birthYear;
+    private String yourMale;
+    private String whereareyoufrom;
+
+    public  User() {
+    }
+    public String getNickName() { return nickName; }
+
+    public void setNickName(String nickname) {this.nickName = nickname;}
+
+    public String getFirstName() { return firstName; }
+
+    public void setFirstName(String firstname) {this.firstName = firstName;}
+
+    public String getLastName() { return lastName; }
+
+    public void setLastName(String lastName) {this.lastName = lastName;}
+
+    public String getYourMale() {return yourMale;}
+
+    public void setWhereareyoufrom(String whereareyoufrom) {this.whereareyoufrom = whereareyoufrom;}
+
+    public String getWhereareyoufrom() {return  whereareyoufrom;}
+
+
+    public void setYourMale(String yourMale) {this.yourMale = yourMale;}
+
+    public int getBirthYear() { return birthYear; }
+
+    public void setBirthYear(int birthYear) {this.birthYear = birthYear; }
+
+
+
+
+
+
+    }
+
+
+
+
